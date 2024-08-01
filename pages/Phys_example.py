0b6afba486b6c42342e7922fd99fb9bfe746201a
Cheeseramen <foxhoony@gmail.com>
2024-08-01 14:14:27 +0900

    Update Phys_example.py

--- pages/Phys_example.py
+++ pages/Phys_example.py
@@ -22,19 +22,21 @@ def to_markdown(text):
 def try_generate_content(api_key, prompt):
     genai.configure(api_key=api_key)
    
-    model = genai.GenerativeModel(model_name="gemini-1.0-pro",
-                                  generation_config={
-                                      "temperature": 0.9,
-                                      "top_p": 1,
-                                      "top_k": 1,
-                                      "max_output_tokens": 2048,
-                                  },
-                                  safety_settings=[
-                                      {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-                                      {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-                                      {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-                                      {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-                                  ])
+    model = genai.GenerativeModel(
+        model_name="gemini-1.0-pro",
+        generation_config={
+            "temperature": 0.9,
+            "top_p": 1,
+            "top_k": 1,
+            "max_output_tokens": 2048,
+        },
+        safety_settings=[
+            {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+            {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+            {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+            {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+        ]
+    )
     try:
         response = model.generate_content(prompt)
         return response.text
